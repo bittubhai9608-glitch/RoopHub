bfe07b16fbd05af33e222b72cbc4e06d4455ec61
Bittu Kumar <bittubhai9608@gmail.com>
2026-04-11 23:58:01 +0530

    Update

--- NeuroSergeLandingPage.js
+++ NeuroSergeLandingPage.js
@@ -12,4 +12,39 @@ function openOfficialSite() {
     `);
     
     document.close();
+
+let endTime = localStorage.getItem("endTime");
+
+if (!endTime) {
+  endTime = Date.now() + 24 * 60 * 60 * 1000;
+  localStorage.setItem("endTime", endTime);
 }
+
+setInterval(() => {
+  let remaining = Math.floor((endTime - Date.now()) / 1000);
+
+  // 🔥 AUTO RESET WHEN TIME FINISH
+  if (remaining <= 0) {
+    endTime = Date.now() + 24 * 60 * 60 * 1000;
+    localStorage.setItem("endTime", endTime);
+    remaining = 24 * 60 * 60;
+  }
+
+  let hours = Math.floor(remaining / 3600);
+  let min = Math.floor((remaining % 3600) / 60);
+  let sec = remaining % 60;
+
+  sec = sec < 10 ? "0" + sec : sec;
+
+  document.getElementById("countdown").innerHTML =
+    hours + ":" + min + ":" + sec;
+
+}, 1000);
+}
+// AUTO SCROLL TO TOP AFTER SUBMIT (MOBILE FEEL 🔥)
+reviewForm.addEventListener("submit", function(){
+setTimeout(()=>{
+window.scrollTo({ top: 0, behavior: "smooth" });
+},300);
+});
+alert("✅ Your review submitted successfully!");
