9da73981145f4d141dc5fdbafb96de35b8643f2b
Bittu Kumar <bittubhai9608@gmail.com>
2026-04-16 18:41:24 +0530

    requirements.txt

--- script.js
+++ script.js
@@ -330,7 +330,7 @@ function sendMessage(){
     renderMessages();
     input.value = "";
 
-    fetch("http://127.0.0.1:5000/chat", {
+    fetch("https://roophub.onrender.com/chat", {
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify({
